53b6273a0e4addc9adbc263df684872a4fb137a7
iAmAdamReid <adam@adamreid.me>
2018-10-31 16:40:58 -0700

    Build skeleton for unit tests

--- index.spec.js
+++ index.spec.js
@@ -2,25 +2,99 @@ const server = require('./api/server.js');
 
 const request = require('supertest');
 
-describe('NOTES API', () => {
-    describe('GET ALL NOTES @ /api/notes', () => {
-        it('should return JSON', async () => {
-            const response = await request(server).get('/api/notes');
+describe('SERVER', () => {
+    describe('USERS API', () => {
+        it('should create a new user', async (done) => {
+            request(server).post('/api/users/register').send({username: 'testUser', password: 'password'}).end(function(err, res){
+                token = res.body.token;
 
-            expect(response.type).toBe('application/json');
+                request(server).get('/api/notes').set('Authorization', token).expect(200).end(function(err, res){
+                    expect(typeof token).toBe('string')
+                    if(err) return done(err);
+                    done();
+                })
+            })
         })
 
-        it('should return status code 401 if not authorized', async () => {
-            const response = await request(server).get('/api/notes');
-            expect(response.status).toBe(401);
+        it('should login the new user', async(done) => {
+            request(server).post('/api/users/login').send({username: 'testUser', password: 'password'}).end(function(err, res){
+                token = res.body.token;
+
+                request(server).get('/api/notes').set('Authorization', token).expect(200).end(function(err, res){
+                    if(err) return done(err);
+                    done();
+                })
+            })
         })
+
+
+        describe('LOGGED IN NOTES API', () => {
+            it('should return a list of notes if logged in', async(done) => {
+                // TODO
+                done();
+            })
+
+            it('should return a note by ID if logged in', async (done) => {
+                // TODO
+                done();
+            })
+
+            it('should allow a logged in user to post a new note', async(done) => {
+                // TODO
+                done();
+            })
+
+            it('should allow a user to edit a note', async(done) => {
+                // TODO
+                done();
+            })
+
+            it('should allow a user to delete a note', async(done) => {
+                // TODO
+                done();
+            })
+        })
+
+
+        it('should edit the user credentials', async(done) => {
+            // check for user edit
+            // TODO
+            done();
+        })
+
+        it('should delete the test user', async (done) => {
+            // check for user deletion
+            // TODO
+            done();
+        })
+
+        describe('LOGGED OUT NOTES API', () => {
+            it('should return 401 if not logged in for GET', async(done) => {
+                // TODO
+                done();
+            })
+
+            it('should return 401 if not logged in for GET BY ID', async(done) => {
+                // TODO
+                done();
+            })
+
+            it('should return 401 if not logged in for POST', async(done) => {
+                // TODO
+                done();
+            })
+
+            it('should return 401 if not logged in for PUT', async(done) => {
+                // TODO
+                done();
+            })
+
+            it('should return 401 if not logged in for DELETE', async(done) => {
+                // TODO
+                done();
+            })
+        })
+
     })
+
 })
-
-    describe('GET NOTE BY ID @ /api/notes/:id', () => {
-        it('should return JSON', async () => {
-            const response = await request(server).get(`/api/notes/${id}`);
-            expect(response.type).toBe('application/json')
-
-        })
-    })
