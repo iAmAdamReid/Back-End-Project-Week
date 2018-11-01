4e2ade704adfb7c9aaaba31a4fd8dedc77d3f677
iAmAdamReid <adam@adamreid.me>
2018-10-31 17:48:34 -0700

    CRUD methods for Users

--- api/server.js
+++ api/server.js
@@ -28,8 +28,9 @@ router.use('/api/notes/', middleware.authenticate);
 server.use(router);
 
 
-
+/****************************************************************************************************/
 /** Generate Token Function **/
+/****************************************************************************************************/
 function generateToken(user) {
     const jwtPayload = {
         ...user,
@@ -39,8 +40,9 @@ function generateToken(user) {
     }
     return jwt.sign(jwtPayload, jwtKey, jwtOptions);
 }
-
+/****************************************************************************************************/
 // REGISTER NEW USERS
+/****************************************************************************************************/
 async function register(req, res) {
     try {
         const creds = req.body;
@@ -67,8 +69,9 @@ async function register(req, res) {
         return res.status(500).json({error: `An error occurred creating a new user.`})
     }
 }
-
+/****************************************************************************************************/
 // LOGIN EXISTING USERS
+/****************************************************************************************************/
 async function login(req, res) {
     try{
         const creds = req.body;
@@ -99,10 +102,12 @@ server.get('/api/authorize', middleware.authenticate, (req, res) => {
 // .insert(note)
 // .update(id, newNote)
 // .remove(id)
-
+/****************************************************************************************************/
 /*** BEGIN NOTES API ***/
-
+/****************************************************************************************************/
+/****************************************************************************************************/
 // Get all notes
+/****************************************************************************************************/
 server.get('/api/notes', (req, res) => {
     notesDb.find().then(notes => {
         return res.status(200).json(notes);
@@ -112,8 +117,9 @@ server.get('/api/notes', (req, res) => {
         return res.status(500).json({error: "An error occured retrieving the notes."});
     })
 })
-
+/****************************************************************************************************/
 // Get note by ID
+/****************************************************************************************************/
 server.get('/api/notes/:id', (req, res) => {
     const id = req.params.id;
     notesDb.findById(id).then(note => {
@@ -128,8 +134,9 @@ server.get('/api/notes/:id', (req, res) => {
         return res.status(500).json({error: `Error retrieving note ${id}`});
     })
 })
-
+/****************************************************************************************************/
 // Add new note
+/****************************************************************************************************/
 
 server.post('/api/notes', (req, res) => {
 
@@ -157,8 +164,9 @@ server.post('/api/notes', (req, res) => {
         return res.status(500).json({error: `An error occured when adding the new note.`});
     })
 })
-
+/****************************************************************************************************/
 // Update existing note
+/****************************************************************************************************/
 server.put('/api/notes/:id', (req, res) => {
     const id = req.params.id;
 
@@ -193,8 +201,9 @@ server.put('/api/notes/:id', (req, res) => {
     })
 })
 
-
+/****************************************************************************************************/
 // delete existing note
+/****************************************************************************************************/
 server.delete('/api/notes/:id', (req, res) => {
     const id = req.params.id;
 
@@ -207,18 +216,143 @@ server.delete('/api/notes/:id', (req, res) => {
         return res.status(500).json({error: `An error occured deleting note ID ${id}.`});
     })
 })
-
+/****************************************************************************************************/
 /*** END NOTES API ***/
+/****************************************************************************************************/
+
+/****************************************************************************************************/
+/*** BEGIN USERS API  ***/
+/****************************************************************************************************/
+
+/****************************************************************************************************/
+// GET ALL USERS
+/****************************************************************************************************/
+
+server.get('/api/users', middleware.authenticate, (req, res) => {
+    console.log(req.decodedToken);
+    usersDb.find().then(users => {
+         // do not show passwords on get requests
+        let cleanUsers = users.map(user => {
+            delete user.password;
+            return user;
+        })
+        if(!users){
+            return res.status(404).json({error: `No Users Found`})
+        } 
+
+        return res.status(200).json({cleanUsers});
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({error: `Error retrieving users.`});
+    })
+})
+
+/****************************************************************************************************/
+// GET USER BY ID
+/****************************************************************************************************/
+
+server.get('/api/users/:id', middleware.authenticate, (req, res) => {
+    const id = req.params.id;
+    usersDb.findById(id).then(user => {
+        // do not show passwords on get requests
+        delete user.password;
+        if(!user){
+            return res.status(404).json({error: `User with ID ${id} does not exist.`})
+        } else {
+            return res.status(200).json(user);
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({error: `Error retrieving user.`})
+    })
+})
+
+
+/****************************************************************************************************/
+// REGISTER NEW USER
+/****************************************************************************************************/
 
 server.post('/api/users/register', async (req, res) => {
     register(req, res);
 })
 
+/****************************************************************************************************/
+// LOGIN EXISTING USER
+/****************************************************************************************************/
+
 server.post('/api/users/login', async (req, res) => {
     login(req, res);
 })
 
-/*** BEGIN USERS API  ***/
+
+/****************************************************************************************************/
+// EDIT EXISTING USER
+/****************************************************************************************************/
+
+server.put('/api/users/:id', middleware.authenticate, (req, res) => {
+    const id = req.params.id;
+    const user_id = req.decodedToken.id;
+
+    const changes = {
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    // hash the password input
+    const hash = bcrypt.hashSync(changes.password, 14);
+    changes.password = hash;
+
+    // if decoded ID matches param ID, or if user type is admin, allow changes
+    if(parseInt(user_id) === parseInt(id) || req.decodedToken.privileges === 'admin'){
+        console.log('match');
+        usersDb.update(id, changes).then(reply => {
+            if(!reply){
+                return res.status(404).json({error: `User not found.`})
+            }
+            return res.status(200).json({message: `User ${id} updated.`})
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({error: `Error updating user.`})
+        })
+    } else {
+        return res.status(401).json({error: `You are not authorized to do that.`})
+    }
+
+    
+})
+
+/****************************************************************************************************/
+// DELETE EXISTING USER
+/****************************************************************************************************/
+
+server.delete('/api/users/:id', middleware.authenticate, (req, res) => {
+    const id = req.params.id;
+    const user_id = req.decodedToken.id;
+    // if privilege is admin, or if user ID matches URL ID, allow deletion
+
+if(parseInt(user_id) === parseInt(id) || req.decodedToken.privileges === 'admin'){
+    usersDb.remove(id).then(reply => {
+        if(!reply){
+            return res.status(404).json({error: `User not found.`})
+        }
+        return res.status(200).json({message: `User ${id} deleted.`})
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({error: `An error occured deleting user ${id}.`})
+    })
+} else {
+    return res.status(401).json({error: `You are not authorized to do that.`})
+}
+})
+
+/****************************************************************************************************/
+// END USERS API
+/****************************************************************************************************/
+
 
 
 server.get('/', (req, res) => {
